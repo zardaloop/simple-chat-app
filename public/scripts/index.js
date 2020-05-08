81aee4395ce664cb26ba61264c33882ada007317
Farzan Majdani <farzan.majdani@gmail.com>
2020-05-08 14:24:01 +0100

    from video to audio
    updated to use audio and run on local

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -63,7 +63,7 @@ function updateUserList(socketIds) {
   });
 }
 
-const socket = io.connect("localhost:5000");
+const socket = io.connect("192.168.0.9:5000");
 
 socket.on("update-user-list", ({ users }) => {
   updateUserList(users);
